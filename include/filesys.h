bf7bef1438bb9e240bb2c27780d25443f7b3db4c
Andrew Chen <andrew.chen923@gmail.com>
2018-04-17 15:07:47 -0400

    added to the header and included file/dir txt file

--- include/filesys.h
+++ include/filesys.h
@@ -8,7 +8,19 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+class Node {
+  string _name;
+  int _size;
+  int _timestamp;
+  bool _folder;
+  map<string,Node> _children;
+
+public:
+  Node(string, int, int, bool);
+};
 
 #endif
