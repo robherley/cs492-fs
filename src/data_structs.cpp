dd44eb64f7fe65860b75bcdf5d17687995356c05
Robert Herley <rherley@stevens.edu>
2018-04-18 20:27:42 -0400

    fuck ben

--- src/data_structs.cpp
+++ src/data_structs.cpp
@@ -6,7 +6,7 @@
  * ----------------------------------------------------------
  */
 
-Node ::Node(string n_name, int n_size, time_t n_timestamp) {
+Node::Node(string n_name, int n_size, time_t n_timestamp) {
   name = n_name;
   size = n_size;
   timestamp = n_timestamp;
@@ -14,30 +14,33 @@ Node ::Node(string n_name, int n_size, time_t n_timestamp) {
   children = n_children;
 }
 
-void Node ::add_child(string n_name, int n_size, time_t n_timestamp) {
+void Node::add_child(string n_name, int n_size, time_t n_timestamp) {
   Node new_node(n_name, n_size, n_timestamp);
   pair<string, Node *> new_pair(n_name, &new_node);
   children.insert(new_pair);
 }
 
-bool Node ::has_child(string child_name) {
+bool Nod::has_child(string child_name) {
   return children.find(child_name) != children.end();
 }
 
 void print_children(ostream &os, Node *node, int level) {
   if (node->children.size() != 0) {
-    unsigned counter = 0;
-    for (auto &child : node->children) {
-      os << setw(level * 4) << setfill(' ') << "";
-      if (counter + 1 == node->children.size())
-        os << "└── ";
-      else
-        os << "├── ";
-      os << BLUE << child.first << GREEN << " [Size: " << (child.second)->size
-         << ", TS: " << (child.second)->timestamp << "]" << RES << endl;
-      if ((child.second)->children.size() != 0)
-        print_children(os, child.second, level + 1);
-      counter++;
+    // unsigned counter = 0;
+    for (auto child : node->children) {
+      // Node curr = *child.second;
+      cout << (child.second)->name << endl;
+      // os << setw(level * 4) << setfill(' ') << "";
+      // if (counter + 1 == node->children.size())
+      //   os << "└── ";
+      // else
+      //   os << "├── ";
+      // os << &curr;
+      // if (curr.children.size() != 0) {
+      //   cout << curr.children.size() << endl;
+      //   // print_children(os, child.second, level + 1);
+      // }
+      // counter++;
     }
   }
 }
@@ -56,14 +59,14 @@ ostream &operator<<(ostream &os, Node *node) {
  * ----------------------------------------------------------
  */
 
-LDisk ::LDisk(int n_block_size, int n_max_size) {
+LDisk::LDisk(int n_block_size, int n_max_size) {
   b_size = n_block_size;
   max_size = n_max_size;
   vector<bool> n_blocks;
   blocks = n_blocks;
 }
 
-void LDisk ::set_avail(int loc, bool availability) {
+void LDisk::set_avail(int loc, bool availability) {
   blocks[loc] = availability;
 }
 
@@ -73,12 +76,12 @@ void LDisk ::set_avail(int loc, bool availability) {
  * ----------------------------------------------------------
  */
 
-LFile ::LFile() {
+LFile::LFile() {
   vector<FileBlock> n_blocks;
   blocks = n_blocks;
 }
 
-void LFile ::add_fb(int block_size, void *ptr) {
+void LFile::add_fb(int block_size, void *ptr) {
   FileBlock fb = {block_size, ptr};
   blocks.push_back(fb);
 }
